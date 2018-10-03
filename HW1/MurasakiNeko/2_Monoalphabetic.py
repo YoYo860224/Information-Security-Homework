6ba595d4282395c1d70038f02fbe2d86eafb6b85
Zeny-Yu, Zeng <B10415041@mail.ntust.edu.tw>
2018-10-03 20:32:21 +0800

    remove case sensitive

--- HW1/MurasakiNeko/2_Monoalphabetic.py
+++ HW1/MurasakiNeko/2_Monoalphabetic.py
@@ -1,17 +1,15 @@
-input = "KeepGoingNeverGiveUp"
-key = {'z':'M','y':'N','x':'B','w':'V','v':'C','u':'X','t':'Z','s':'L','r':'K','q':'J','p':'H','o':'G','n':'F' }
-key.update({'m':'D','l':'S','k':'A','j':'P','i':'O','h':'I','g':'U','f':'Y','e':'T','d':'R','c':'E','b':'W','a':'Q'})
+input = "keepgoingnevergiveup"
+key = {'z': 'M', 'y': 'N', 'x': 'B', 'w': 'V', 'v': 'C', 'u': 'X',
+       't': 'Z', 's': 'L', 'r': 'K', 'q': 'J', 'p': 'H', 'o': 'G', 'n': 'F'}
+key.update({'m': 'D', 'l': 'S', 'k': 'A', 'j': 'P', 'i': 'O', 'h': 'I',
+            'g': 'U', 'f': 'Y', 'e': 'T', 'd': 'R', 'c': 'E', 'b': 'W', 'a': 'Q'})
 
 print("input:", input, "key:", key)
 
 # Encryption
 encryption = ""
 for char in input:
-    if ord(char) >= ord('a'):
-        base = ord('a')
-    else:
-        base = ord('A')
-    encryption += chr(ord(key[char.lower()]) - ord('A') + base)
+    encryption += key[char.lower()]
 print("encryption:", encryption)
 # Decryption
 inverseKey = {}
@@ -21,9 +19,5 @@ for elementKey, element in key.items():
 
 decryption = ""
 for char in encryption:
-    if ord(char) >= ord('a'):
-        base = ord('a')
-    else:
-        base = ord('A')
-    decryption += chr(ord(inverseKey[char.upper()]) - ord('a') + base)
+    decryption += inverseKey[char.upper()].upper()
 print("decryption:", decryption)
