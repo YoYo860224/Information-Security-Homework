78499fd6fec773b897df7221520dee76826205f3
IDEA-NB\MurasakiNeko <z557898812z@gmail.com>
2018-10-04 09:06:23 +0800

    temp version

--- HW1/MurasakiNeko/3_Playfair.py
+++ HW1/MurasakiNeko/3_Playfair.py
@@ -23,6 +23,9 @@ for char in temp:
     playfairKey[char] = index
     index += 1
 playfairKey['J'] = playfairKey['I']
+playfairKeyInverse = [i for i in range(25)]
+for k, v in playfairKey.items():
+    playfairKeyInverse[v] = k
 # print(playfairKey)
 # for i in range(5):
 #     for j in range(5):
@@ -35,11 +38,22 @@ playfairKey['J'] = playfairKey['I']
 newInput = []
 index = 0
 while index < len(input):
-    if index + 1 == len(input) or input[index] == input[index + 1]:
-        newInput.append(input[index] + 'X')
+    if index + 1 == len(input) or input[index].upper() == input[index + 1].upper():
+        newInput.append(input[index].upper() + 'X')
         index += 1
     else:
-        newInput.append(input[index] + input[index + 1])
+        newInput.append(input[index].upper() + input[index + 1].upper())
         index += 2
-print(newInput)
+# print(newInput)
+encryption = ""
+for string in newInput:
+    firstIndex = [playfairKey[string[0]] // 5, playfairKey[string[0]] % 5]
+    secondIndex = [playfairKey[string[1]] // 5, playfairKey[string[1]] % 5]
+    if firstIndex[0] != secondIndex[0] and firstIndex[1] != secondIndex[1]:
+        encryption += playfairKeyInverse[firstIndex[0] * 5 + secondIndex[1]] + playfairKeyInverse[firstIndex[1] * 5 + secondIndex[0]]
+    elif firstIndex[1] == secondIndex[1]:
+        encryption += playfairKeyInverse[(firstIndex[0] + 1) % 5 * 5 + firstIndex[1]] + playfairKeyInverse[(secondIndex[1] + 1) % 5 * 5 + firstIndex[0]]
+    else:
+        encryption += playfairKeyInverse[firstIndex[0] * 5 + (firstIndex[1] + 1) % 5] + playfairKeyInverse[firstIndex[0] * 5 + (secondIndex[1] + 1) % 5]
+print(encryption)
 # Decryption
